3a6ecc630ef3ada9cd8414e22fd2874743bb78d5
Rav3nFK <shabanin.serzh@mail.ru>
2021-10-19 09:47:09 +0300

    Добавьте файлы проекта.

---
+++ WPFForme/Pages/UsersListPG.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFForme.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для UsersListPG.xaml
+    /// </summary>
+    public partial class UsersListPG : Page
+    {
+        public UsersListPG()
+        {
+            InitializeComponent();
+            lbUsersList.ItemsSource = BaseConnect.BaseModel.users.ToList();
+        }
+
+        private void lbTraits_Loaded(object sender, RoutedEventArgs e)
+        {
+            ListBox lb = (ListBox)sender;
+            int id = Convert.ToInt32(lb.Uid);
+            lb.ItemsSource = BaseConnect.BaseModel.users_to_traits.Where(x => x.id_user == id).ToList();
+            lb.DisplayMemberPath = "traits.trait";
+        }
+
+        private void Changebtn_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)sender;
+            int id = Convert.ToInt32(b.Uid);
+            auth tUser = BaseConnect.BaseModel.auth.FirstOrDefault(x => x.id == id);
+            LoadCl.MFrame.Navigate(new ChangePG(tUser));
+        }
+
+        private void Delbtn_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)sender;
+            int id = Convert.ToInt32(b.Uid);
+            auth DellUs = BaseConnect.BaseModel.auth.FirstOrDefault(x => x.id == id);
+            BaseConnect.BaseModel.auth.Remove(DellUs);
+            BaseConnect.BaseModel.SaveChanges();
+            MessageBox.Show("Пользователь удален!");
+            TimeSpan.FromSeconds(3);
+            lbUsersList.ItemsSource = BaseConnect.BaseModel.users.ToList();
+        }
+
+        private void createbtn_Click(object sender, RoutedEventArgs e)
+        {
+            LoadCl.MFrame.Navigate(new reg());
+        }
+
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            LoadCl.MFrame.GoBack();s
+        }
+    }
+}
---
+++ WPFForme/Pages/reg.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFForme.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для reg.xaml
+    /// </summary>
+    public partial class reg : Page
+    {
+        public reg()
+        {
+            InitializeComponent();
+            list1.ItemsSource = BaseConnect.BaseModel.genders.ToList();
+            list1.SelectedValuePath = "id";
+            list1.DisplayMemberPath = "gender";
+
+            string[] traits2 = new string[3];
+            List<traits> traits1 = BaseConnect.BaseModel.traits.ToList();
+            int i = 0;
+            foreach (traits tr in traits1)
+            {
+                traits2[i] = tr.trait;
+                i++;
+            }
+            //dobr.Content = traits2[0];
+            //nezh.Content = traits2[1];
+            //lask.Content = traits2[2];
+
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            LoadCl.MFrame.GoBack();
+        }
+
+        private void Zapbut_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                auth LogPass = new auth() { login = Logtxt.Text, password = Passtxt.Password, role = 2 };
+                BaseConnect.BaseModel.auth.Add(LogPass);
+                BaseConnect.BaseModel.SaveChanges();
+
+
+                users NewUs = new users();
+                NewUs.name = Nametxt.Text;
+                NewUs.id = LogPass.id;
+                NewUs.gender = (int)list1.SelectedValue;
+                NewUs.dr = (DateTime)Datedr.SelectedDate;
+
+                BaseConnect.BaseModel.users.Add(NewUs);
+
+
+                if (dobr.IsChecked == true)
+                {
+                    users_to_traits UTT = new users_to_traits();
+                    UTT.id_user = NewUs.id;
+                    UTT.id_trait = 1;
+                    BaseConnect.BaseModel.users_to_traits.Add(UTT);
+                }
+                if (nezh.IsChecked == true)
+                {
+                    users_to_traits UTT = new users_to_traits();
+                    UTT.id_user = NewUs.id;
+                    UTT.id_trait = 2;
+                    BaseConnect.BaseModel.users_to_traits.Add(UTT);
+
+                }
+                if (lask.IsChecked == true)
+                {
+                    users_to_traits UTT = new users_to_traits();
+                    UTT.id_user = NewUs.id;
+                    UTT.id_trait = 3;
+                    BaseConnect.BaseModel.users_to_traits.Add(UTT);
+
+                }
+                BaseConnect.BaseModel.SaveChanges();
+
+                MessageBox.Show("Пользователь " + Logtxt.Text + ", успешно зарегистрирован");
+            }
+            catch
+            {
+                MessageBox.Show("Произошла ошибка");
+            }
+        }
+    }
+}
